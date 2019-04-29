538ad146b70bc1f9ea7aa0a666de6fbc6e9bbc66
Jack Ellis <jack.ellis@godaddy.com>
2019-04-29 20:26:34 +0100

    fix(vue-compose): branch hoc renders correctly for multiple components
    a param rewrite was messing up rendering for more than one component
    
    fixes https://github.com/jackmellis/vue-hoc/issues/44

--- packages/vue-compose/spec/branch.spec.js
+++ packages/vue-compose/spec/branch.spec.js
@@ -59,3 +59,31 @@ test('falls back to original component when false', t => {
 
   t.is(html, '<div>original component</div>');
 });
+
+test('creates a branch HOC for multiple components', t => {
+  const enhance = branch(
+    (props) => props.flag === true,
+    (h) => h('div', null, [ 'true' ]),
+  );
+
+  const A = enhance({
+    props: [ 'flag' ],
+    render: h => h('div', [ 'A' ]),
+  });
+  const B = enhance({
+    props: [ 'flag' ],
+    render: h => h('div', [ 'B' ]),
+  });
+  const C = enhance({
+    props: [ 'flag' ],
+    render: h => h('div', [ 'C' ]),
+  });
+
+  const a = mount(A, { props: { flag: false }}).$html;
+  const b = mount(B, { props: { flag: false }}).$html;
+  const c = mount(C, { props: { flag: false }}).$html;
+
+  t.is(a, '<div>A</div>');
+  t.is(b, '<div>B</div>');
+  t.is(c, '<div>C</div>');
+});
